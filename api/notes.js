24c15ce87057c20c8e57ccb49bf510bc0266b5c6
DavianaGu <jennifer1107@126.com>
2025-11-12 01:00:01 +0800

    Delete api directory

--- api/notes.js
+++
@@ -1,7 +0,0 @@
-// api/notes.js
-export default function handler(req, res) {
-  res.status(200).json([
-    { id: 1, title: "测试1", content: "你好，这是从后端返回的数据" },
-    { id: 2, title: "测试2", content: "如果能看到，说明 API 正常" },
-  ]);
-}
